9088953e4d195f5bb52d23e0e5cc85722fa2590f
段昊辰 <duanhaochen@126.com>
2021-05-10 18:11:46 +0800

    feat: 初始化项目

---
+++ packages/main/gulpfile.mjs
@@ -0,0 +1,47 @@
+import process from 'process';
+import path from 'path';
+import gulp from 'gulp';
+import terser from 'gulp-terser';
+import typescript from 'gulp-typescript';
+import changed from 'gulp-changed';
+import plumber from 'gulp-plumber';
+import { requireJson, metaHelper } from '@sweet-milktea/utils';
+
+const { __dirname } = metaHelper(import.meta.url);
+const baseTypescriptConfig = await requireJson(path.join(__dirname, '../../tsconfig.json'));
+const tsConfig = await requireJson(path.join(__dirname, './tsconfig.json'));
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isWatch = process.env.WATCH;
+
+function devTsProject() {
+  const result = gulp.src('src/**/*.{ts,tsx}')
+    .pipe(changed('lib'))
+    .pipe(plumber())
+    .pipe(typescript({
+      ...baseTypescriptConfig.compilerOptions,
+      ...tsConfig.compilerOptions
+    }));
+
+  return result.js.pipe(gulp.dest('lib'));
+}
+
+function watch() {
+  gulp.watch('src/**/*.{ts,tsx}', devTsProject);
+}
+
+function tsProject() {
+  const result = gulp.src('src/**/*.{ts,tsx}')
+    .pipe(typescript({
+      ...baseTypescriptConfig.compilerOptions,
+      ...tsConfig.compilerOptions
+    }));
+
+  return result.js
+    .pipe(terser({ ecma: 2020 }))
+    .pipe(gulp.dest('lib'));
+}
+
+export default isDevelopment
+  ? (isWatch ? gulp.series(devTsProject, watch) : devTsProject)
+  : tsProject;
---
+++ packages/oicq-desktop/sweet.config.ts
@@ -0,0 +1,90 @@
+import * as process from 'process';
+import * as path from 'path';
+import AntdDayjsWebpackPlugin from 'antd-dayjs-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+const isDev: boolean = process.env.NODE_ENV === 'development';
+const analyzer: boolean = process.env.ANALYZER === 'true';
+
+// html代码压缩配置
+const htmlWebpackPluginMinify: boolean | object = isDev ? false : {
+  collapseWhitespace: true,
+  keepClosingSlash: true,
+  removeComments: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  useShortDoctype: true,
+  minifyCSS: true,
+  minifyJS: {
+    ecma: 2020,
+    safari10: true
+  }
+};
+
+/**
+ * 模块使用node的commonjs的方式引入
+ * @param { Array<string> } node: node模块名称
+ */
+function nodeExternals(node: Array<string>): { [k: string]: string } {
+  const result: { [k: string]: string } = {};
+
+  for (const name of node) {
+    result[name] = `globalThis.require('${ name }')`;
+  }
+
+  return result;
+}
+
+export default function(info: object): { [key: string]: any } {
+  const plugins: Array<any> = [
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]
+  ];
+
+  if (!isDev) {
+    plugins.unshift(['transform-react-remove-prop-types', { mode: 'remove', removeImport: true }]);
+  }
+
+  const config: { [key: string]: any } = {
+    frame: 'react',
+    dll: [
+      'react',
+      'react-dom',
+      'prop-types',
+      '@reduxjs/toolkit',
+      'react-redux',
+      'reselect',
+      'react-router',
+      'react-router-dom',
+      'history'
+    ],
+    entry: {
+      index: [path.join(__dirname, 'src/index.tsx')]
+    },
+    html: [{ template: path.join(__dirname, 'src/index.pug'), minify: htmlWebpackPluginMinify }],
+    externals: nodeExternals([
+      '@electron/remote',
+      'electron',
+      'oicq'
+    ]),
+    js: {
+      ecmascript: true,
+      plugins,
+      exclude: /node_modules/i
+    },
+    ts: {
+      configFile: isDev ? 'tsconfig.json' : 'tsconfig.prod.json',
+      plugins,
+      exclude: /node_modules/
+    },
+    sass: {
+      include: /src/
+    },
+    css: {
+      include: /node_modules[\\/](_?antd)/
+    },
+    plugins: [new AntdDayjsWebpackPlugin()].concat(analyzer ? [new BundleAnalyzerPlugin()] : [])
+  };
+
+  return config;
+}
